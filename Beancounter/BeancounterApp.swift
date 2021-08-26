9232e07be025820b7082082820b6f02c417495cd
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-08-26 10:50:51 -0600

    Initial Commit

---
+++ Beancounter/BeancounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeancounterApp.swift
+//  Beancounter
+//
+//  Created by Adam Garrett-Harris on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BeancounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
